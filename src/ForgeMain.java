2f253e198fcc93fd2ae24f8f16ce16f4b5293a4b
welchs1423 <welchs1423@naver.com>
2026-03-18 02:50:10 +0900

    feat: complete memory alignment and padding demo

--- src/ForgeMain.java
+++ src/ForgeMain.java
@@ -1,24 +1,30 @@
 import java.lang.foreign.Arena;
+import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 
 void main() {
-    // 1. Arena를 통해 메모리 관리 (try-with-resources로 자동 해제)
+    // int(4바이트) + 패딩(4바이트) + double(8바이트) = 총 16바이트
+    var userLayout = MemoryLayout.structLayout(
+            ValueLayout.JAVA_INT.withName("id"),
+            MemoryLayout.paddingLayout(4),
+            ValueLayout.JAVA_DOUBLE.withName("score")
+    );
+
     try (Arena arena = Arena.ofConfined()) {
+        MemorySegment segment = arena.allocate(userLayout);
 
-        // 2. 8바이트 메모리 할당 (C의 malloc과 유사)
-        MemorySegment segment = arena.allocate(8L);
+        // id는 0번 인덱스
+        segment.set(ValueLayout.JAVA_INT, 0, 1001);
 
-        // 3. 메모리에 직접 long 값 쓰기
-        segment.set(ValueLayout.JAVA_LONG, 0, 20260318L);
-
-        // 4. 메모리에서 값 읽어오기
-        long value = segment.get(ValueLayout.JAVA_LONG, 0);
+        // score는 int(4) + 패딩(4) 이후인 8번 인덱스
+        segment.set(ValueLayout.JAVA_DOUBLE, 8, 98.5);
 
         System.out.println("---------------------------------");
-        System.out.println("Memory Address: " + segment.address());
-        System.out.println("Read Value: " + value);
+        System.out.println("Total size: " + userLayout.byteSize() + " bytes");
+        System.out.println("ID: " + segment.get(ValueLayout.JAVA_INT, 0));
+        System.out.println("Score: " + segment.get(ValueLayout.JAVA_DOUBLE, 8));
         System.out.println("---------------------------------");
-        System.out.println("Modern Off-Heap Forge Start");
+        System.out.println("OffHeap-Forge: Success with Padding");
     }
 }
